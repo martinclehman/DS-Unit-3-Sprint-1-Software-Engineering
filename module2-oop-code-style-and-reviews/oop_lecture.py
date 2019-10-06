7bca22a2b398ee57021bbe7efd66e3d6cd55f527
martinclehman <martinclehman@users.noreply.github.com>
2019-10-06 02:05:07 -0400

    update 312 notes

--- module2-oop-code-style-and-reviews/oop_lecture.py
+++ module2-oop-code-style-and-reviews/oop_lecture.py
@@ -1,7 +1,35 @@
 '''
 DSPT1-Lecture 312-OOP and Code Style
 '''
+from random import random
 
 def print_list(numberList):
     for item in numberList:
         print(f'Item {item}')
+
+class Game:
+    """General representation of a game"""
+    def __init__(self, player1='Tina', player2='Raul'):
+        self.round = 1
+        self.player1 = player1
+        self.player2 = player2
+
+    def print_players(self):
+        """Print the players of the game"""
+        print('{} is playing {}'.format(self.player1, self.player2))
+
+    def add_rounds(self):
+        """Incrementing the number of rounds by 1"""
+        self.round += 1
+
+    def winner(self):
+        return(self.player1 if random() > 0.5 else self.player2)
+
+class TicTacToe(Game):
+    def __init__(self, player1 = 'Joe', player2='Bill'):
+        super().__init__(player1, player2)
+
+    # def print_players(self):
+    #     """Print the players of the game"""
+    #     print('{} is playing Tic-Tac-Toe with {}'.format(self.player1,
+    #                                                      self.player2))
